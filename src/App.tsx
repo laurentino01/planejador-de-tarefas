7b0e784ec63a4d0010d85c1f5c1132104467de77
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-07-04 19:19:55 -0300

    refactor(header): Created an component for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React, { useEffect } from "react";
 import { ListaDeTarefas } from "./pages/ListaDeTarefas";
 import "./sass/header.scss";
-
-import { NavLink } from "react-router-dom";
+import { HeaderNav } from "./layouts";
 
 function App() {
   useEffect(() => {}, []);
 
   return (
     <main className="App ">
-      <header className="container header ">
-        <h6>Organização</h6>
-        <nav>
-          <NavLink to={"/"}>Tarefas</NavLink>
-        </nav>
-      </header>
+      <HeaderNav />
 
       <ListaDeTarefas />
     </main>
--- src/layouts/HeaderNav.tsx
+++ src/layouts/HeaderNav.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../sass/header.scss";
 
 export const HeaderNav = () => {
   return (
-    <header>
-      <h1>teste</h1>
+    <header className="container header ">
+      <h6>Organização</h6>
+      <nav>
+        <NavLink to={"/"}>Tarefas</NavLink>
+      </nav>
     </header>
   );
 };
