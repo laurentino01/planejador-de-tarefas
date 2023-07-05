e2fdd27d493941d94c9f6a7b9cf974874c0eef8c
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-07-05 16:17:41 -0300

    feat(headernav): Created darkmode button

--- src/layouts/HeaderNav.tsx
+++ src/layouts/HeaderNav.tsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../sass/header.scss";
-import { Icon, Switch, SwitchProps } from "@mui/material";
+import Switch from "@mui/joy/Switch";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import { Grid, SxProps } from "@mui/material";
+import { Box } from "@mui/system";
+
+const darkModeSwitchStyleSX: SxProps = {
+  "--Switch-thumbSize": "20px",
+};
 
 export const HeaderNav = () => {
+  const [check, setCheck] = useState(false);
+
+  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheck(e.target.checked);
+  };
+
   return (
     <header className="container header ">
       <h6>Organização</h6>
+
       <nav>
         <NavLink to={"/"}>Tarefas</NavLink>
       </nav>
 
-      <div>
-        <Switch
-          checkedIcon={<Brightness7Icon />}
-          className="muiSwitch"
-          defaultChecked
-        />
-      </div>
+      <Switch
+        checked={check}
+        onChange={handleCheck}
+        sx={darkModeSwitchStyleSX}
+        slotProps={{
+          thumb: {
+            input: { "aria-label": "dark mode" },
+            children: check ? (
+              <Brightness7Icon fontSize="small" color="primary" />
+            ) : (
+              <Brightness2Icon fontSize="small" />
+            ),
+          },
+        }}
+      />
     </header>
   );
 };
