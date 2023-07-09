10017b7ea1359273aa830763001617ead7870aa1
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-07-09 00:11:49 -0300

    feat(toggleDarkmode)

--- src/layouts/HeaderNav.tsx
+++ src/layouts/HeaderNav.tsx
@@ -17,7 +17,31 @@ import { SunIcon } from "../components/SunIcon";
 import { MoonIcon } from "../components/MoonIcon";
 
 const switchStyle: SxProps = {
-  "& .css-1nn0rgv-MuiButtonBase-root-MuiSwitch-switchBase:hover": {},
+  "& .MuiButtonBase-root ": {
+    padding: "1px",
+    top: "30%",
+    left: "10px",
+  },
+  "& .MuiButtonBase-root.Mui-checked ": {
+    padding: "1px",
+    top: "30%",
+    left: "10px",
+  },
+  "& .MuiSwitch-track": {
+    width: "70px",
+    height: "20px",
+    borderRadius: "13px",
+    opacity: 1,
+    backgroundColor: "black",
+  },
+  "& .css-147uapr-MuiButtonBase-root-MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track ":
+    {
+      width: "70px",
+      height: "20px",
+      borderRadius: "13px",
+      opacity: 1,
+      backgroundColor: "white",
+    },
 };
 
 export const HeaderNav = () => {
