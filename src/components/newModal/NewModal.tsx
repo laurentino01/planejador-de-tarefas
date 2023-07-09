1ead6c73ff50842f7ffb2e95810773effa826ba4
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-07-09 19:27:39 -0300

    refactor(newmodal)

--- src/components/newModal/NewModal.tsx
+++ src/components/newModal/NewModal.tsx
@@ -1,15 +1,29 @@
-import { Box, Button, Modal, SxProps, TextField } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
 import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  SxProps,
+  TextField,
+  Typography,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { useForm } from "react-hook-form";
+
 import { ITarefaModuleProps } from "../../interfaces/ITarefaModuleProps";
 import { TarefasServices } from "../../services/tarefasServices/TarefasServices";
 
 const style: SxProps = {
   position: "absolute" as "absolute",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "20px",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  maxWidth: 600,
+  width: " 90%",
   bgcolor: "background.paper",
   borderRadius: "15px",
   boxShadow: 24,
@@ -20,16 +34,25 @@ export const NewModal = ({ closeModal, targetTarefa }: ITarefaModuleProps) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const onSubmit = (data) => console.log(getValues());
 
-  const handleCreate = (title, description) => {
+  /* const handleCreate = (title, description) => {
     TarefasServices.create({ title, description });
-  };
+  }; */
 
   return (
     <>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <Button onClick={handleOpen} sx={{ backgroundColor: "white" }}>
+        Open modal
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -37,34 +60,53 @@ export const NewModal = ({ closeModal, targetTarefa }: ITarefaModuleProps) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <CloseIcon onClick={handleClose}></CloseIcon>
-          <h2>Criar Nova Tarefa</h2>
+          <CloseIcon
+            sx={{ alignSelf: "end", fontSize: "32px", cursor: "pointer" }}
+            onClick={handleClose}
+          ></CloseIcon>
+          <Typography component={"h2"} variant="h4">
+            Criar Nova Tarefa
+          </Typography>
 
-          <form onSubmit={() => handleCreate(title, description)}>
-            <div>
+          <Box
+            component={"form"}
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              alignItems: "center",
+            }}
+          >
+            <Box>
               <TextField
+                {...(register("titulo"), { require: true })}
                 id="outlined-basic"
                 label="Título"
                 placeholder="Digite um título. "
                 required
-                onChange={(e) => setTitle(e.target.value)}
+
+                /*  onChange={(e) => setTitle(e.target.value)} */
               />
-            </div>
-            <div>
+              {errors.exampleRequired && <span>This field is required</span>}
+            </Box>
+            <Box>
               <TextField
+                {...(register("description"), { require: true })}
                 id="outlined-multiline-static"
                 label="Descrição"
                 multiline
                 rows={4}
                 placeholder="Digite uma descrição."
                 required
-                onChange={(e) => setDescription(e.target.value)}
+                /* onChange={(e) => setDescription(e.target.value)} */
               />
-            </div>
+              {errors.exampleRequired && <span>This field is required</span>}
+            </Box>
             <Button type="submit" variant="contained" disableElevation>
               Criar Tarefa
             </Button>
-          </form>
+          </Box>
         </Box>
       </Modal>
     </>
